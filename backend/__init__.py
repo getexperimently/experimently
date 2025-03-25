4c45505074b7b424fc79d8cd403ed2a486cb7123
amarkanday <a.markanday@gmail.com>
2025-03-25 15:38:31 -0700

    auth and exp serrvice

---
+++ backend/__init__.py
@@ -0,0 +1,11 @@
+import os
+from logging.config import fileConfig
+from alembic import context
+from sqlalchemy import engine_from_config, pool, text
+import sys
+from pathlib import Path
+from backend.app.core.config import settings
+
+database_url = os.getenv("DATABASE_URI", str(settings.DATABASE_URI))
+
+print(f"\n Alembic using DB URI: {database_url}\n")
